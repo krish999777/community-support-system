666e84226625f0624a3d81ff2268495a9c871f61
Krish Shah <krishcshah09@gmail.com>
2026-04-23 17:40:20 +0530

    changed api_url for deployment(render)

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001';
+const API_URL = 'https://community-support-system-rbrz.onrender.com:5001';
 
 const API = axios.create({
   baseURL: API_URL,
